3ae6b76f0db946ea46f0f5bc1335ef54dbebcd1b
lak-ibm <lak@ibm.com>
2025-12-03 16:05:44 -0800

    Delete benchmarking directory

--- benchmarking/LocalLoop.c
+++
@@ -1,9 +0,0 @@
-#include "native/helper_Loop.h"
-#include <stdio.h>
-#include <limits.h>
-
-int main (){
-    printf("Local ");
-    navtiveLoopCounterHelper(INT_MAX);
-    return 0;
-}
--- benchmarking/native/helper_Loop.h
+++
@@ -1,6 +0,0 @@
-#ifndef HELPER_LOOP_H
-#define HELPER_LOOP_H
-
-void navtiveLoopCounterHelper(int num);
-
-#endif 
